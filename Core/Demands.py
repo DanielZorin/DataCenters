5153ff4070e30f77b52b80b4cfa8c5caabfef8cd
Daniel A. Zorin <juan@lvk.cs.msu.su>
2012-09-11 14:59:34 +0400

    Demand interface
    Demand classes
    Export to XML

--- Core/Demands.py
+++ Core/Demands.py
@@ -1,13 +1,57 @@
 class VM:
-    pass
+    number = -1
+    def __init__(self, id, speed):
+        self.id = id
+        self.speed = speed
 
 class DemandStorage:
-    pass
+    number = -1
+    def __init__(self, id, volume, type):
+        self.id = id
+        self.volume = volume
+        self.type = type
 
 class DemandLink:
-    pass
+    def __init__(self, e1, e2):
+        self.e1 = e1
+        self.e2 = e2
 
 class Demand:
-    pass
+    def __init__(self, id):
+        self.id = id
+        self.vertices = []
+        self.edges = []
+
+    def AddLink(self, e):
+        self.edges.append(e)
+
+    def AddVertex(self, v):
+        self.vertices.append(v)
+        v.number = len(self.vertices)
+
+    def ExportToXml(self):
+        dom = xml.dom.minidom.Document()
+        root = dom.createElement("demand")
+        root.setAttribute("id", "0")
+        dom.appendChild(root)
+        for v in self.vertices:
+            if isinstance(v, VM):
+                tag = dom.createElement("vm")
+                tag.setAttribute("speed", v.speed)
+            elif isinstance(v, DemandStorage):
+                tag = dom.createElement("storage")
+                tag.setAttribute("volume", v.volume)
+            tag.setAttribute("number", v.number)
+            tag.setAttribute("name", v.name)
+            root.appendChild(tag)
+        for v in self.edges:
+            tag = dom.createElement("link")
+            tag.setAttribute("from", v.e1.number)
+            tag.setAttribute("to", v.e2.number)
+            root.appendChild(tag)
+        return dom.toprettyxml()
+
+    def GenerateRandom(self):
+        pass
 
 
