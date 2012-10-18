1111aee78321c746cc2b0ddd7b56070154babcf2
Ivan Zotov <tridcatov@gmail.com>
2012-10-18 17:47:39 +0400

    alg: completed algorithm interface with status
    Class usage example in root folder

--- Algorithm/common/algorithm.h
+++ Algorithm/common/algorithm.h
@@ -1,25 +1,43 @@
 #ifndef ALGORITHM_H
 #define ALGORITHM_H
 
-#include <vector>
+#include <set>
 
+class Request;
 class Network;
+class Assignment;
 
 class Algorithm
 {
 public:
+    class ResultEnum {
+    public:
+        enum Result {
+            SUCCESS = 0,
+            PARTIAL,
+            FAILURE = -1
+        };
+    };
+public:
+    typedef std::set<Request *> Requests;
+    typedef std::set<Assignment *> Assignments;
 
 private:
     Algorithm();
-public:
-    Algorithm(Network * n) {
-        network = n;
-    }
 
 public:
-    virtual void schedule() = 0;
+    Algorithm(Network * n, Requests const & r)
+        : network(n)
+        , requests(r)
+    {}
+public:
+    virtual Assignments getAssignments() { return assignments; }
+public:
+    virtual ResultEnum::Result schedule() = 0;
 protected:
     Network * network;
+    Requests requests;
+    Assignments assignments;
 };
 
 #endif // ALGORITHM_H
--- Algorithm/compilation.cpp
+++ Algorithm/compilation.cpp
@@ -1,5 +1,18 @@
 #include "algorithm.h"
+#include "network.h"
+#include "request.h"
+
+#include <set>
+#include <string>
 
 int main(int argc, char ** argv){
+    Network * network = new Network();
+    Request * r1 = new Request();
+    Request * r2 = new Request();
+    Algorithm::Requests requests;
+    requests.insert(r1);
+    requests.insert(r2);
+    Algorithm::ResultEnum::Result result = Algorithm::ResultEnum::SUCCESS; 
+     
     return 0;
 }
