9b20c25ff559d0b14e6f5dac449cd072fa971335
Daniel A. Zorin <juan@lvk.cs.msu.su>
2012-12-13 14:07:34 +0300

    mainwindow (something doesn't work, wtf?)
    Signed-off-by: Daniel A. Zorin <juan@lvk.cs.msu.su>

--- Algorithm/main.cpp
+++ Algorithm/main.cpp
@@ -1,82 +1,7 @@
-#include "xmlconverter.h"
-#include "algorithmdispatcher.h"
-
-#include "algorithm.h"
-
 #include <iostream>
 using std::cerr;
 using std::endl;
-
-#include <QtCore/QString>
-#include <QtCore/QFile>
-#include <QtCore/QIODevice>
-#include <QtCore/QTextStream>
-
-#include <iostream>
-#include <streambuf>
-#include <string>
-
-#include <QtGui/QTextEdit>
-#include <QtGui/QApplication>
-#include <QtGui/QMainWindow>
-
-class QDebugStream : public std::basic_streambuf<char>
-{
-public:
- QDebugStream(std::ostream &stream, QTextEdit* text_edit) : m_stream(stream)
- {
-  log_window = text_edit;
-  m_old_buf = stream.rdbuf();
-  stream.rdbuf(this);
- }
- ~QDebugStream()
- {
-  // output anything that is left
-  if (!m_string.empty())
-   log_window->append(m_string.c_str());
-
-  m_stream.rdbuf(m_old_buf);
- }
-
-protected:
- virtual int_type overflow(int_type v)
- {
-  if (v == '\n')
-  {
-   log_window->append(m_string.c_str());
-   m_string.erase(m_string.begin(), m_string.end());
-  }
-  else
-   m_string += v;
-
-  return v;
- }
-
- virtual std::streamsize xsputn(const char *p, std::streamsize n)
- {
-  m_string.append(p, p + n);
-
-  int pos = 0;
-  while (pos != std::string::npos)
-  {
-   pos = m_string.find('\n');
-   if (pos != std::string::npos)
-   {
-    std::string tmp(m_string.begin(), m_string.begin() + pos);
-    log_window->append(tmp.c_str());
-    m_string.erase(m_string.begin(), m_string.begin() + pos + 1);
-   }
-  }
-
-  return n;
- }
-
-private:
- std::ostream &m_stream;
- std::streambuf *m_old_buf;
- std::string m_string;
- QTextEdit* log_window;
-};
+#include "mainwindow_moc.h"
 
 int main(int argc, char ** argv)
 {
@@ -87,46 +12,6 @@ int main(int argc, char ** argv)
     }
 
 	QApplication app(argc, argv);
-	QMainWindow* mw = new QMainWindow();
-	QTextEdit* myTextEdit = new QTextEdit(mw);
-	QDebugStream qout(std::cout, myTextEdit);
-	QDebugStream qout2(std::cerr, myTextEdit);
-	mw->show();
-
-	std::cout << "Starting algorithm" << endl;
-
-    QString inputName = QString(argv[1]);
-    QString outputName = argc > 2 ? QString(argv[2]) : inputName;
-    QString algorithmType = argc > 3 ? QString(argv[3]) : QString("d");
-    
-    QString input;
-    {
-        QFile inputFile(inputName);
-        inputFile.open(QIODevice::ReadOnly | QIODevice::Text);
-        QTextStream inputStream(&inputFile);
-        input = inputStream.readAll();
-        inputFile.close();
-    }
-
-    XMLConverter converter(input);
-    Network * network = converter.getNetwork();
-    Requests requests = converter.getRequests();
-
-    Algorithm * algorithm = AlgorithmDispatcher::Dispatch(algorithmType, network, requests); 
-    algorithm->schedule();
-
-    Assignments assignments = algorithm->getAssignments();
-    converter.setAssignments(assignments);
-
-    {
-        QFile outputFile(outputName);
-        outputFile.open(QIODevice::WriteOnly | QIODevice::Text);
-        QTextStream outputStream(&outputFile);
-        outputStream << converter.getMixdownContent();
-        outputFile.close();
-    }
-    
-    
-    delete algorithm;
+	OurMainWindow* mw = new OurMainWindow(argc, argv);
 	return app.exec();
 }
---
+++ Algorithm/mainwindow.h
@@ -0,0 +1,101 @@
+#include "xmlconverter.h"
+#include "algorithmdispatcher.h"
+
+#include "algorithm.h"
+
+#include <iostream>
+using std::cerr;
+using std::endl;
+
+#include <QtCore/QString>
+#include <QtCore/QFile>
+#include <QtCore/QIODevice>
+#include <QtCore/QTextStream>
+
+#include <iostream>
+#include <streambuf>
+#include <string>
+
+#include <QtGui/QTextEdit>
+#include <QtGui/QApplication>
+#include <QtGui/QMainWindow>
+#include <QtGui/QVBoxLayout>
+#include <QtGui/QPushButton>
+#include <QtGui/QProgressBar>
+#include <QtCore/QTimer>
+#include <QtCore/QObject>
+#include <QtGui/QWidget>
+#include "qdebugstream.h"
+
+class OurMainWindow : public QMainWindow
+{
+	Q_OBJECT
+
+	Algorithm* algorithm;
+	XMLConverter* converter;
+	QString outputName;
+	QPushButton* ok;
+	QTextEdit* myTextEdit;
+public:
+	OurMainWindow(int argc, char** argv) : QMainWindow()
+	{
+		QVBoxLayout* layout = new QVBoxLayout(centralWidget());
+		
+		myTextEdit = new QTextEdit(this);
+		ok = new QPushButton("Close", this);
+		ok->setEnabled(false);
+		layout->addWidget(myTextEdit);
+		layout->addWidget(ok);
+		setCentralWidget(new QWidget());
+		centralWidget()->setLayout(layout);
+		QDebugStream qout(std::cout, myTextEdit);
+		QDebugStream qout2(std::cerr, myTextEdit);
+
+		std::cout << "Starting algorithm" << endl;
+		QString inputName = QString(argv[1]);
+		outputName = argc > 2 ? QString(argv[2]) : inputName;
+		QString algorithmType = argc > 3 ? QString(argv[3]) : QString("d");
+    
+		QString input;
+		{
+			QFile inputFile(inputName);
+			inputFile.open(QIODevice::ReadOnly | QIODevice::Text);
+			QTextStream inputStream(&inputFile);
+			input = inputStream.readAll();
+			inputFile.close();
+		}
+
+		converter = new XMLConverter(input);
+		Network * network = converter->getNetwork();
+		Requests requests = converter->getRequests();
+
+		Algorithm * algorithm = AlgorithmDispatcher::Dispatch(algorithmType, network, requests); 
+		QTimer* timer = new QTimer();
+		timer->setInterval(1000);
+		timer->setSingleShot(true);
+		timer->start();
+		QObject::connect(timer, SIGNAL(timeout()), SLOT(run()));
+		QObject::connect(ok, SIGNAL(clicked()), SLOT(close()));
+		show();
+	}
+
+public slots:
+	void run()
+	{
+		algorithm->schedule();
+		Assignments assignments = algorithm->getAssignments();
+		converter->setAssignments(assignments);
+
+		{
+			QFile outputFile(outputName);
+			outputFile.open(QIODevice::WriteOnly | QIODevice::Text);
+			QTextStream outputStream(&outputFile);
+			outputStream << converter->getMixdownContent();
+			outputFile.close();
+		}
+		delete converter;
+		delete algorithm;
+		ok->setEnabled(true);
+
+	}
+};
