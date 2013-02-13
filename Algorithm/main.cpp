a095141167e47ccc284197fd34c08ace3a4489c8
Alevtina Glonina <alevtina@lvk.cs.msu.su>
2013-02-14 01:00:29 +0400

    unused code deleted
    Signed-off-by: Alevtina Glonina <alevtina@lvk.cs.msu.su>

--- Algorithm/main.cpp
+++ Algorithm/main.cpp
@@ -10,47 +10,8 @@ int main(int argc, char ** argv)
         cerr << "Usage: " << argv[0] << " <input_file> [output_file = input_file] [algorithm = [a|c|d]]" << endl;
         return 1;
     }
-	if (true)
-	{
-		QApplication app(argc, argv);
-		OurMainWindow* mw = new OurMainWindow(argc, argv);
-		return app.exec();
-	}
-	else
-	{
-		QString inputName = QString(argv[1]);
-		QString outputName = argc > 2 ? QString(argv[2]) : inputName;
-		QString algorithmType = argc > 3 ? QString(argv[3]) : QString("d");
-    
-		QString input;
-		{
-			QFile inputFile(inputName);
-			inputFile.open(QIODevice::ReadOnly | QIODevice::Text);
-			QTextStream inputStream(&inputFile);
-			input = inputStream.readAll();
-			inputFile.close();
-		}
-
-		XMLConverter converter(input);
-		Network * network = converter.getNetwork();
-		Requests requests = converter.getRequests();
-
-		Algorithm * algorithm = AlgorithmDispatcher::Dispatch(algorithmType, network, requests); 
-		algorithm->schedule();
-
-		Assignments assignments = algorithm->getAssignments();
-		converter.setAssignments(assignments);
-
-		{
-			QFile outputFile(outputName);
-			outputFile.open(QIODevice::WriteOnly | QIODevice::Text);
-			QTextStream outputStream(&outputFile);
-			outputStream << converter.getMixdownContent();
-			outputFile.close();
-		}
-    
-    
-		delete algorithm;
-		return 0;
-	}
+	
+	QApplication app(argc, argv);
+	OurMainWindow* mw = new OurMainWindow(argc, argv);
+	return app.exec();
 }
