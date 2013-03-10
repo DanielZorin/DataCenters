1cc62ecf1b2874687bfa1b39c5b280686b035cc0
Daniel A. Zorin <juan@lvk.cs.msu.su>
2013-03-10 11:34:25 +0300

    generation draft
    Signed-off-by: Daniel A. Zorin <juan@lvk.cs.msu.su>

--- plugins/IdealGenerator.py
+++ plugins/IdealGenerator.py
@@ -1,7 +1,10 @@
 import random
+from Core.Resources import ResourceGraph, Computer, Storage, Router
+from Core.Demands import *
 
 class IdealGenerator:
     storagePercent = 50
+    compPercent = 50
     number = 10
 
     def __init__(self):
@@ -12,7 +15,49 @@ class IdealGenerator:
         return "Ideal"
 
     def Generate(self, resources):
-        median = self.storagePercent / self.number
+        medianStorage = self.storagePercent / self.number
+        medianComp = self.compPercent / self.number
+        comps = []
+        storages = []
+        for v in resources.vertices:
+            if isinstance(v, Computer):
+                comps += [v.speed]
+            elif isinstance(v, Storage):
+                storages += [v.volume]
+        requests = []
+        sumSt = 0
+        sumComp = 0
+        compTotal = int(sum(comps) * float(self.compPercent) / 100.0)
+        stTotal = int(sum(storages) * float(self.storagePercent) / 100.0)
+        while (sumSt < stTotal) and (sumComp < compTotal):
+            st = int(max([0, int(random.gauss(medianStorage, 2))]) * sum(storages) / 100.0)
+            comp = int(max([0, int(random.gauss(medianComp, 2))]) * sum(comps) / 100.0)
+            sumSt += st
+            sumComp += comp
+            requests += [[st, comp]]
+        res = []
+        for r in requests:
+            d = Demand("demand_" + str(r))
+            sumSt = 0
+            while sumSt < r[0]:
+                st = min([max(storages), random.randint(1, r[0] / 3 + 1)])
+                for i in range(len(storages)):
+                    if storages[i] > st:
+                        storages[i] -= st
+                        break
+                d.AddVertex(DemandStorage("storage", st, 0, 9000))
+                sumSt += st
+            sumComp = 0
+            while sumComp < r[1]:
+                st = min([max(comps), random.randint(1, r[1] / 3 + 1)])
+                for i in range(len(comps)):
+                    if comps[i] > st:
+                        comps[i] -= st
+                        break
+                d.AddVertex(VM("vm", st))
+                sumComp += st
+
+        return res
 
     def GetSettings(self):
         # importing here to allow using the class without Qt
@@ -24,7 +69,8 @@ class IdealGenerator:
             def getTranslatedSettings(self):
                 return [
                 [self.tr("Request count"), self.parent.number],
-                [self.tr("Storage"), self.parent.storagePercent]
+                [self.tr("Storage"), self.parent.storagePercent],
+                [self.tr("Computers"), self.parent.compPercent]
                         ]
         t = Translator(self)
         return t.getTranslatedSettings()
@@ -33,6 +79,7 @@ class IdealGenerator:
         # importing here to allow using the class without Qt
         self.number = dict[0][1]
         self.storagePercent = dict[1][1]
+        self.compPercent = dict[2][1]
 
 def pluginMain():
     return IdealGenerator
