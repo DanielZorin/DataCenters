26ed39aa92cb1828776b27d66326f9b9628132f9
Ivan Zotov <tridcatov@gmail.com>
2013-11-28 00:09:49 +0400

    Initial cleanup for massive refactor

---
+++ algo/main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char ** argv)
+{
+    if ( argc < 3 || argc > 5 )
+    {
+        printf("Usage: %s <input_file> <-w/-c> [output_file = input_file] [algorithm = [c|d]]\n", *argv);
+        return 1;
+    }
+
+    return 0;
+}
