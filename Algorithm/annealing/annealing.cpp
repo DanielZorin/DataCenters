8d42c1c41b84c57c428c02f8579f57312f93f098
Alexandra Shostik <shostix@mail.ru>
2014-03-03 12:49:13 +0400

    Fixed bug in acception of new assignment tests: /assets/openstack/RandomFit/serial/
    Signed-off-by: Alexandra Shostik <shostix@mail.ru>

--- Algorithm/annealing/annealing.cpp
+++ Algorithm/annealing/annealing.cpp
@@ -50,20 +50,21 @@ void Annealing::cleanUpAssignment(Assignment * assignment)
 
 Algorithm::Result Annealing::generateVMAssignment(Request *request)
 {
+	//cout << "		generate VM assignment" << endl;
 	Nodes &vm = request->getVirtualMachines();
 	Nodes &nodes = Algorithm::network->getNodes();
 	vector<Node *> vectorNodes(nodes.begin(), nodes.end());
-	for (Nodes::iterator i = vm.begin(); i != vm.end(); ++i) {
+	for (Nodes::iterator iter = vm.begin(); iter != vm.end(); ++iter) {
 		Result res = FAILURE;
 		int counter = 0;
-		Node * vm = *i;
+		Node * vm = *iter;
 		while (res == FAILURE && counter < 10000) {
 			int i = rand() % nodes.size();
 		    res = tryToAssignVM(vm, vectorNodes[i]);
 		    ++counter;
 		}
+		//cout << "		counter: " << counter << endl;
 		if (res == FAILURE) {
-			cleanUpAssignment(currentAssignment);
 			return FAILURE;
 		}
 	}
@@ -72,33 +73,38 @@ Algorithm::Result Annealing::generateVMAssignment(Request *request)
 
 Algorithm::Result Annealing::generateStorageAssignment(Request *request)
 {
+	//cout << "		generate storage assignment" << endl;
 	Stores & storages = request->getStorages();
-	Stores & stores = network->getStores();
+	Stores & stores = Algorithm::network->getStores();
 	vector<Store *> vectorStores(stores.begin(), stores.end());
-	for (Stores::iterator i = storages.begin(); i != storages.end(); ++i) {
+	//if (storages.size() < 1) {
+		//cout << "storages.size() = 0" << endl;
+	//}
+	for (Stores::iterator iter = storages.begin(); iter != storages.end(); ++iter) {
+		cout << "cycle..." << endl;
 		Result res = FAILURE;
 		int counter = 0;
-		Store * st = *i;
+		Store * st = *iter;
 		while (res == FAILURE && counter < 10000) {
+			cout << "..." << endl;
 			int i = rand() % stores.size();
 		    res = tryToAssignStorage(st, vectorStores[i]);
 		    ++counter;
 		}
 		if (res == FAILURE) {
-			cleanUpAssignment(currentAssignment);
+			//cout << "		fail" << endl;
 			return FAILURE;
 		}
+		//cout << "		counter: " << counter << endl;
 	}
 	return SUCCESS;
 }
 
 Algorithm::Result Annealing::generateAssignment(Request *request)
 {
-	if (currentAssignment) {
-		cleanUpAssignment(currentAssignment);
-	}
-	*currentAssignment = Assignment(request);
+	currentAssignment = new Assignment(request);
 	if (generateVMAssignment(request) == FAILURE) {
+		//cout << "	failed to genVM" << endl;
 		return FAILURE;
 	}
 	if (generateStorageAssignment(request) == FAILURE) {
@@ -109,11 +115,11 @@ Algorithm::Result Annealing::generateAssignment(Request *request)
 
 Algorithm::Result Annealing::generateCurAssignments()
 {
-    curAssignments.clear();
+	curAssignments.clear();
     for (Requests::iterator i = requests.begin(); i != requests.end(); i++)
     {
+		//cout << "===================="<< endl;
         Request *request = *i;
-        Algorithm::Result result = FAILURE;
         if (generateAssignment(request) == SUCCESS) {
 			curAssignments.insert(currentAssignment);
 		}
@@ -127,8 +133,8 @@ Algorithm::Result Annealing::generatePrevAssignments()
     prevAssignments.clear();
     for (Requests::iterator i = requests.begin(); i != requests.end(); i++)
     {
+		//cout << "	===================="<< endl;
         Request *request = *i;
-        Algorithm::Result result = FAILURE;
         if (generateAssignment(request) == SUCCESS) {
 			prevAssignments.insert(currentAssignment);
 		}
@@ -138,34 +144,29 @@ Algorithm::Result Annealing::generatePrevAssignments()
 
 Algorithm::Result Annealing::schedule()
 {
-	Result result = FAILURE;
-	while (result == FAILURE)
-         result = generatePrevAssignments();
+    Result result = generatePrevAssignments();
     assignments = prevAssignments;
-	
+	cout << "Requests: " << requests.size() << endl;
 	double delta = 0, temperature, start_temperature = 50;
 	double p = 1; 
 	int step = 0;
 	do {
-		//std::cout << "step " << step << std::endl;
+		std::cout << "step " << step << std::endl;
 		temperature = start_temperature / log(1 + step);
 		for (int i = 0; i < 10; ++i) {
-			result = FAILURE;
-			while (result == FAILURE) {
-			     result = generateCurAssignments();
-			 }
+			result = generateCurAssignments();
 			delta = prevAssignments.size() - curAssignments.size(); 
 			double h = (double)rand() / RAND_MAX;
 			p = exp(-delta / temperature);
 			if (delta <= 0 || h > p) { // cтало лучше
 			    prevAssignments = curAssignments;
 			}
-			if (prevAssignments.size() < assignments.size()) {
+			if (prevAssignments.size() > assignments.size()) {
 				assignments = prevAssignments;
 			}
 		}
 		++step;
-	} while (temperature > 7 && assignments.size() != requests.size() && step < 5000);
+	} while (temperature > 7 && assignments.size() != requests.size() && step < 5);
 	
     printf("Assigned total of %d from %d requests", assignments.size(), requests.size());
     if ( assignments.size() == requests.size() )
