13530693873814ca602cf9f04335ce4671929a35
Alexandra Shostik <shostix@mail.ru>
2014-03-20 17:12:23 +0400

    annealing.h

---
+++ Algorithm/annealing/annealing.h
@@ -0,0 +1,43 @@
+#ifndef ANNEALING_H
+#define ANNEALING_H
+
+#include "common/publicdefs.h"
+#include "common/algorithm.h"
+
+class Annealing : public Algorithm
+{
+private:
+    Annealing();
+    
+    Result tryToAssignVM(Node * vm, Node * node);
+    Result tryToAssignStorage(Store * storage, Store * store);
+    
+    void cleanUpAssignment(Assignment * assignment);
+    
+    Result generateVMAssignment(Request *request, Network *cnetwork);
+    Result generateStorageAssignment(Request *request, Network *cnetwork);
+    
+    Result generateAssignment(Request *request, Network *cnetwork);
+    
+    Result generateCurAssignments();
+    Result generatePrevAssignments();
+    
+    Result tryToInsertNewAssignment(Network *cnetwork);
+    Result changeAssignments(Network *cnetwork);
+    Result changeCurAssignments();
+    
+public:
+    Annealing(Network * n, Requests const & r);
+
+    virtual ~Annealing() {}
+    virtual Algorithm::Result schedule();
+
+    Assignment *currentAssignment;
+    Assignments curAssignments;
+	Assignments prevAssignments;
+	
+	Network *curNetwork;
+	Network *prevNetwork;
+};
+
+#endif
