d7e4119386cd82fd229f4718fe1f9a5ecb7513ad
Daniel A. Zorin <juan@lvk.cs.msu.su>
2014-05-27 14:51:30 +0400

    new xml format
    Signed-off-by: Daniel A. Zorin <juan@lvk.cs.msu.su>

--- Core/Tenant.py
+++ Core/Tenant.py
@@ -140,18 +140,18 @@ class Tenant(AbstractGraph):
         for v in self.vertices:
             if isinstance(v, VM):
                 tag = dom.createElement("vm")
-                tag.setAttribute("vm_name", v.id)
+                tag.setAttribute("name", v.id)
                 tag.setAttribute("image_id", v.image)
                 if v.assigned:
-                    tag.setAttribute("server_name", v.assigned.id)
+                    tag.setAttribute("assignedTo", v.assigned.id)
             elif isinstance(v, Storage):
                 tag = dom.createElement("st")
-                tag.setAttribute("st_name", v.id)
+                tag.setAttribute("name", v.id)
                 if v.assigned:
-                    tag.setAttribute("storage_name", v.assigned.id)
+                    tag.setAttribute("assignedTo", v.assigned.id)
             elif isinstance(v, NetElement):
                 tag = dom.createElement("netelement")
-                tag.setAttribute("netelement_name", v.id)
+                tag.setAttribute("name", v.id)
                 tag.setAttribute("netelement_type", v.type)
                 tag.setAttribute("ip", v.ip)
                 tag.setAttribute("is_router", "1" if v.router else "0")
@@ -160,28 +160,28 @@ class Tenant(AbstractGraph):
                 tag.setAttribute("provider_name", v.provider)
                 tag.setAttribute("external_port", v.port)
                 if v.assigned:
-                    tag.setAttribute("netelement_name", v.assigned.id)
+                    tag.setAttribute("assignedTo", v.assigned.id)
             elif isinstance(v, Domain):
                 tag = dom.createElement("domain")
-                tag.setAttribute("domain_name", v.id)
+                tag.setAttribute("name", v.id)
                 tag.setAttribute("commutation_type", v.type)
                 if v.assigned:
-                    tag.setAttribute("server_name", v.assigned.id)
+                    tag.setAttribute("assignedTo", v.assigned.id)
             elif isinstance(v, Vnf):
                 tag = dom.createElement("vnf")
-                tag.setAttribute("vnf_name", v.id)
+                tag.setAttribute("name", v.id)
                 tag.setAttribute("vnf_type", v.type)
                 tag.setAttribute("profile_type", v.profile)
                 tag.setAttribute("is_service", "1" if v.isservice else "0")
                 tag.setAttribute("service_name", v.servicename)
                 tag.setAttribute("user_name", v.username)
                 if v.assigned:
-                    tag.setAttribute("server_name", v.assigned.id)
+                    tag.setAttribute("assignedTo", v.assigned.id)
                 conset = dom.createElement("exported_connection_set")
                 conset.setAttribute("number_of_ports", str(len(v.connectionset)))
                 for s in v.connectionset:
                     port = dom.createElement("port")
-                    port.setAttribute("port_name", s)
+                    port.setAttribute("name", s)
                     conset.appendChild(port)
                 tag.appendChild(conset)
             if v.x:
@@ -193,7 +193,7 @@ class Tenant(AbstractGraph):
             conset.setAttribute("number_of_ports", str(len(v.ports)))
             for s in v.ports:
                 port = dom.createElement("port")
-                port.setAttribute("port_name", s)
+                port.setAttribute("name", s)
                 conset.appendChild(port)
             tag.appendChild(conset)
             pset = dom.createElement("parameter_set")
@@ -201,7 +201,7 @@ class Tenant(AbstractGraph):
                 param = dom.createElement("parameter")
                 param.setAttribute("parameter_name", p.name)
                 param.setAttribute("parameter_type", p.type)
-                param.setAttribute("value_user", p.value)
+                param.setAttribute("parameter_value", p.value)
                 pset.appendChild(param)
             tag.appendChild(pset)
             nodes.appendChild(tag)
@@ -211,15 +211,14 @@ class Tenant(AbstractGraph):
             tag = dom.createElement("link")
             tag.setAttribute("service", "1" if v.service else "0")
             tag.setAttribute("channel_capacity", str(v.capacity))
-            nd = dom.createElement("node1")
-            nd.setAttribute("node_name", v.e1.id)
-            nd.setAttribute("port_name", v.port1)
-            tag.appendChild(nd)
-            nd = dom.createElement("node2")
-            nd.setAttribute("node_name", v.e2.id)
-            nd.setAttribute("port_name", v.port2)
-            tag.appendChild(nd)
-            tag.appendChild(nd)
+            tag.setAttribute("node1", v.e1.id)
+            tag.setAttribute("port1", v.port1)
+            tag.setAttribute("node2", v.e2.id)
+            tag.setAttribute("port2", v.port2)
+            if v.assigned:
+                res = v.assigned.e1.id + ":" + v.assigned.port1
+                for e in v.assigned:
+                    res += ";" + v.assigned.e2.id + ":" + v.assigned.port2
             links.appendChild(tag)
         root.appendChild(links)
         return root
@@ -252,13 +251,13 @@ class Tenant(AbstractGraph):
                     for port in v.childNodes:
                         if isinstance(port, xml.dom.minidom.Text):
                             continue
-                        s = port.getAttribute("port_name")
+                        s = port.getAttribute("name")
                         ports.append(s)
                 if v.nodeName == "exported_connection_set":
                     for port in v.childNodes:
                         if isinstance(port, xml.dom.minidom.Text):
                             continue
-                        s = port.getAttribute("port_name")
+                        s = port.getAttribute("name")
                         conset.append(s)
                 if v.nodeName == "parameter_set":
                     for param in v.childNodes:
@@ -266,41 +265,37 @@ class Tenant(AbstractGraph):
                             continue
                         name = param.getAttribute("parameter_name")
                         type = param.getAttribute("parameter_type")
-                        value = param.getAttribute("value_user")
+                        value = param.getAttribute("parameter_value")
                         params.append([name, type, value])
             if vertex.nodeName == "vm":
-                v = VM(vertex.getAttribute("vm_name"))
-                v.image = vertex.getAttribute("image_id")
-                assigned = vertex.getAttribute("server_name")             
+                v = VM(vertex.getAttribute("name"))
+                v.image = vertex.getAttribute("image_id")            
             elif vertex.nodeName == "st":
-                v = Storage(vertex.getAttribute("st_name"))
-                assigned = vertex.getAttribute("storage_name")
+                v = Storage(vertex.getAttribute("name"))
             elif vertex.nodeName == "netelement":
                 tag = vertex
-                v = NetElement(tag.getAttribute("netelement_name"))              
+                v = NetElement(tag.getAttribute("name"))              
                 v.type = tag.getAttribute("netelement_type")
                 v.ip = tag.getAttribute("ip")
                 v.router = tag.getAttribute("is_router") == 1
                 v.isservice = tag.getAttribute("is_service") == 1
                 v.servicename = tag.getAttribute("service_name")
                 v.provider = tag.getAttribute("provider_name")
-                v.port = tag.getAttribute("external_port")
-                assigned = vertex.getAttribute("switch_name") 
+                v.port = tag.getAttribute("external_port") 
             elif vertex.nodeName == "vnf":
                 v = Vnf()
                 tag = vertex
-                v.id = tag.getAttribute("vnf_name")
+                v.id = tag.getAttribute("name")
                 v.type = tag.getAttribute("vnf_type")
                 v.profile = tag.getAttribute("profile_type")
                 v.isservice = tag.getAttribute("is_service") == "1"
                 v.servicename = tag.getAttribute("service_name")
                 v.username = tag.getAttribute("user_name")
                 v.connectionset = conset
-                assigned = vertex.getAttribute("server_name") 
             elif vertex.nodeName == "domain":
-                v = Domain(vertex.getAttribute("domain_name"))
+                v = Domain(vertex.getAttribute("name"))
                 v.type = vertex.getAttribute("commutation_type")
-                assigned = vertex.getAttribute("server_name") 
+            assigned = vertex.getAttribute("assignedTo") 
             x = vertex.getAttribute("x")
             y = vertex.getAttribute("y")
             if x != '':
@@ -320,18 +315,13 @@ class Tenant(AbstractGraph):
     def ParseLinks(self, root, resources):
         for edge in root.childNodes:
             if edge.nodeName == "link":
-                for v in edge.childNodes:
-                    if isinstance(v, xml.dom.minidom.Text):
-                        continue
-                    if v.tagName == "node1":
-                        src = v.getAttribute("node_name")
-                        port1 = v.getAttribute("port_name")
-                    if v.tagName == "node2":
-                        dst = v.getAttribute("node_name")
-                        port2 = v.getAttribute("port_name")
+                src = edge.getAttribute("node1")
+                port1 = edge.getAttribute("port1")
+                dst = edge.getAttribute("node2")
+                port2 = edge.getAttribute("port2")
                 cap = int(edge.getAttribute("channel_capacity"))
                 service = edge.getAttribute("service") == "1"
-                assigned = edge.getAttribute("assigned")
+                assigned = edge.getAttribute("assignedTo")
                 # TODO: error handling
                 srcv = [v for v in self.vertices if v.id == src][0]
                 dstv = [v for v in self.vertices if v.id == dst][0]
