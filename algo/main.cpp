39db731246a135c31667df855fcd8d01c5210c0f
Ivan Zotov <tridcatov@gmail.com>
2014-07-01 13:03:25 +0400

    Added return codes, partial assignment designated

--- algo/main.cpp
+++ algo/main.cpp
@@ -20,12 +20,19 @@
 #include "operation.h"
 #include "criteria.h"
 
+enum ReturnCodes {
+   SUCCESS = 0,
+   PARTIAL_FAILURE,
+   INVALID_INPUT,
+   INVALID_USAGE
+};
+
 int main(int argc, char ** argv)
 {
     if ( argc != 3 )
     {
         printf("Usage: %s <input file> <output file>\n", *argv);
-        return 1;
+        return INVALID_USAGE;
     }
 
     QFile input(argv[1]);
@@ -38,7 +45,7 @@ int main(int argc, char ** argv)
     int errorLine, errorColumn;
     if ( !document.setContent(inputStream.readAll(), false, &errMessage, &errorLine, &errorColumn)) {
         printf("XML parsing error, reason: %s at line %d, column %d\n", errMessage.toStdString().c_str(), errorLine, errorColumn);
-        return 2;  
+        return INVALID_INPUT;  
     }
 
     QDomElement root = document.documentElement();
@@ -46,9 +53,6 @@ int main(int argc, char ** argv)
     QDomElement resources = root.elementsByTagName("resources").item(0).toElement();
     ResourcesXMLFactory resourcesFactory = ResourcesXMLFactory(resources);
     Network * network= resourcesFactory.getNetwork();
-    if ( network != 0 ) {
-    	printf("Network has %d elements\n", network->getElements().size());
-    }
 
     QDomNodeList tenants = root.elementsByTagName("tenant");
     Requests requests;
@@ -62,7 +66,6 @@ int main(int argc, char ** argv)
 			tenantsFactory.push_back(sampleFactory);
 			clientFactory.push_back(sampleFactory);
 			Request * request = sampleFactory->getRequest();
-			printf("Request has %d elements\n", request->getElements().size());
 			requests.insert(request);
     	}
     }
@@ -73,7 +76,6 @@ int main(int argc, char ** argv)
             TenantXMLFactory* sampleFactory = new TenantXMLFactory(tenants.item(i).toElement());
             tenantsFactory.push_back(sampleFactory);
             Request * request = sampleFactory->getRequest();
-            printf("Request has %d elements\n", request->getElements().size());
             requests.insert(request);
 
             // Parsing external links
@@ -111,8 +113,9 @@ int main(int argc, char ** argv)
     QTextStream outStream(&output);
     outStream << document.toString(4);
 
-    printf("Node assigned: %d of %d requests\n", nodeAssignedRequests, requests.size());
-    printf("Model assigned: %d of %d requests\n", assignedRequests, requests.size());
+    if ( nodeAssignedRequests != requests.size())
+       return PARTIAL_FAILURE;
 
-    return 0;
+    return SUCCESS;
+
 }
