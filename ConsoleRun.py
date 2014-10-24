8ef8cfe82bf13da81504f5f34dceed6f41e235c5
Daniel A. Zorin <juan@lvk.cs.msu.su>
2014-10-24 12:08:45 +0400

    Console interface for SA algorithm
    Signed-off-by: Daniel A. Zorin <juan@lvk.cs.msu.su>

---
+++ ConsoleRun.py
@@ -0,0 +1,21 @@
+import sys
+
+from DCGUI.Project import *
+from Methods.SimulatedAnnealing import *
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("Usage: python ConsoleRun.py <input> [<output>]")
+        sys.exit(1)
+    filename = sys.argv[1]
+    if len(sys.argv) > 2:
+        outfile = sys.argv[2]
+    else:
+        outfile = filename
+    proj = Project()
+    proj.Load(filename)
+    alg = SimulatedAnnealing(proj)
+    alg.Run()
+    proj.Save(outfile)
+
+
